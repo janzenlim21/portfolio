95073d13d78f40a530a794f625714d3b55ce684e
Janzen Lim <janzen0821lim@gmail.com>
2020-05-25 00:23:42 +1000

    Updates

---
+++ precache-manifest.39acdd6929949f432a24a54464c6d972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d92a14017d108819d3e2b6e9a03e992",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9fed8f51f5378c2d9901",
+    "url": "/portfolio/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/portfolio/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fed8f51f5378c2d9901",
+    "url": "/portfolio/static/js/main.a2b695d9.chunk.js"
+  },
+  {
+    "revision": "f17c31db22189496e46c",
+    "url": "/portfolio/static/js/runtime-main.d83c5850.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/janzenlim21/portfolio/precache-manifest.c432f13996e6ec6fe551319d3dd2f1d8.js"
+  "/portfolio/precache-manifest.39acdd6929949f432a24a54464c6d972.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/janzenlim21/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
