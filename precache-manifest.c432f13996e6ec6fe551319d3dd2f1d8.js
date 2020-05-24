0206e8021106794e4e7b42790cdeab9f9d933d0e
Janzen Lim <janzen0821lim@gmail.com>
2020-05-24 23:57:05 +1000

    Updates

---
+++ precache-manifest.c432f13996e6ec6fe551319d3dd2f1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e500f851a4487e5a8020bf40471adaa3",
+    "url": "/janzenlim21/portfolio/index.html"
+  },
+  {
+    "revision": "c07a7a2293abe1c971a7",
+    "url": "/janzenlim21/portfolio/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/janzenlim21/portfolio/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/janzenlim21/portfolio/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07a7a2293abe1c971a7",
+    "url": "/janzenlim21/portfolio/static/js/main.07743f10.chunk.js"
+  },
+  {
+    "revision": "2b80454a902a874f305a",
+    "url": "/janzenlim21/portfolio/static/js/runtime-main.f04b526e.js"
+  }
+]);
